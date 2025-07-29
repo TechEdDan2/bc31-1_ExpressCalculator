8e73dde0ad814b2a852456847b5eac8a28cd4796
DNel2 <dnel2@Dannys-MacBook-Air.local>
2025-07-29 13:45:38 -0400

    Updated some documentation for the expressError class

--- expressError.js
+++ expressError.js
@@ -1,3 +1,12 @@
+/**
+ * Custom error class for Express applications
+ * that extends the built-in Error class.
+ * It includes a message and a status code.
+ * 
+ * @param {string} message - The error message.
+ * @param {number} status - The HTTP status code.   
+ *  
+ */
 class ExpressError extends Error {
     constructor(message, status) {
         super();
